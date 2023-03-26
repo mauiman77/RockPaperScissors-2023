67efc115b3df10222045cb5c6d14b7464441a3b6
mauiman77 <boerbasher@protonmail.com>
2023-03-26 15:04:04 +0100

    Add Player Choice function

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function getComputerChoice() {
     const computerHand = ["Rock", "Paper", "Scissors"];
     return computerHand[Math.floor(Math.random() * computerHand.length)];
 }
+
+function getPlayerChoice() {
+    const playerHand = prompt("Enter Rock or Paper or Scissors");
+    return playerHand;
+}
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        return 'Computer Wins'
+    }
+    else {
+        return 'Ok test'
+    }
+}
